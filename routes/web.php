d09ef159fbf47464939b79c8d583a5b9c4fec45c
dss-alec <alecdss17@gmail.com>
2025-10-25 18:53:28 -0400

    Atualizacao das rotas
    rotas para as novas paginas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fale-conosco', function () {
+    return view('fale-conosco');
+}) ->name('fale-conosco');
+
+Route::get('/sobre-nos', function () {
+    return view('sobre-nos');
+}) ->name('sobre-nos');
+
+Route::get('/produtos', function () {
+    return view('produtos');
+}) ->name('produtos');
+
+Route::get('/servicos', function () {
+    return view('servicos');
+}) ->name('servicos');
