dea12dae51c90802dbf5f6f001a9138a39dd6eb0
dss-alec <alecdss17@gmail.com>
2025-10-29 21:23:29 -0400

    rotas atualizadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+//Login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+//Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+//Rotas
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/fale-conosco', function () {
+    return view('fale-conosco');
+}) ->name('fale-conosco');
+
+Route::get('/sobre-nos', function () {
+    return view('sobre-nos');
+}) ->name('sobre-nos');
+
+Route::get('/produtos', function () {
+    return view('produtos');
+}) ->name('produtos');
+
+Route::get('/servicos', function () {
+    return view('servicos');
+}) ->name('servicos');
+
+//Rota protegida do administrador
+Route::get('/admin', function () {
+    return "Bem-vindo à área administrativa";
+})->middleware('admin');
