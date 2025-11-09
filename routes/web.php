7cbcd2d1bcaecc6e476e573a905c3647229ce2e2
dss-alec <alecdss17@gmail.com>
2025-11-09 09:19:10 -0400

    rotas com alta coesa

--- routes/web.php
+++ routes/web.php
@@ -5,67 +5,41 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\MensagemController;
 use Illuminate\Support\Facades\Route;
 
-//ROTAS DO USUARIO
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+//rotas do usuario, apenas com view
+Route::view('/', 'welcome')->name('welcome');
+Route::view('sobre-nos', 'sobre-nos')->name('sobre-nos');
+Route::view('servicos', 'servicos')->name('servicos');
+Route::view('fale-conosco', 'fale-conosco')->name('fale-conosco');
 
-Route::get('sobre-nos', function() {
-    return view('sobre-nos');
-})->name('sobre-nos');
-
+// Rotas com lógica de controller
 Route::get('produtos', [ProdutoController::class, 'index'])->name('produtos');
-
-Route::get('servicos', function() {
-    return view('servicos');
-})->name('servicos');
-
-Route::get('fale-conosco', function() {
-    return view('fale-conosco');
-})->name('fale-conosco');
-
 Route::post('fale-conosco', [MensagemController::class, 'store'])->name('fale-conosco.store');
 
+//usuario admin
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//rota gerada pelo breezer
+// Rotas de Perfil (Geradas pelo Breeze)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//grupo de rotas do admin e parao CRUD
+
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    
     Route::get('/dashboard', function () {
         return redirect()->route('admin.cadastro.produtos');
     })->name('dashboard');
 
-    //rota para pagina de cadastro
     Route::get('/cadastro-produtos', [ProdutoController::class, 'paginaDeCadastro'])->name('cadastro.produtos');
-
-    //rota para criar 
-    Route::get('/produtos/criar', [ProdutoController::class, 'create'])->name('produtos.criar');
-
-    //salvar no banco de dados
-    Route::post('/produtos', [ProdutoController::class, 'store'])->name('produtos.store');
-
-    //rota para editar
-    Route::get('/produtos/{produto}/editar', [ProdutoController::class, 'edit'])->name('produtos.editar');
-
-    //salvar no banco de dados
-    Route::put('/produtos/{produto}/atualizar', [ProdutoController::class, 'update'])->name('produtos.atualizar');
-
-    //rota para remover
-    Route::delete('/produtos/{produto}/remover', [ProdutoController::class, 'destroy'])->name('produtos.remover');
-
-    //rota para visualizar mensagens
     Route::get('/visualizar-mensagens', [MensagemController::class, 'visualizacaoMensagens'])->name('mensagens');
 
+    //subtituicao das rotas do CRUD por uma unica
+    Route::resource('produtos', ProdutoController::class)->except(['index', 'show']);
+
 });
-
-
 
 require __DIR__.'/auth.php';
